fa523d144a3baa272ac83c509e6a4b18e7cc6008
air_flyer <fly_in_wind@qq.com>
2016-05-02 00:23:45 +0800

    can swith activity

---
+++ app/src/main/java/com/wxm/keepaccount/IncomeRecordActivity.java
@@ -0,0 +1,26 @@
+package com.wxm.keepaccount;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class IncomeRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_income_record);
+    }
+
+
+    /*
+       确认输入数据
+    */
+    public void onClickOk(View view)    {
+        Intent data=new Intent();
+        setResult(20, data);
+
+        finish();
+    }
+}
--- app/src/main/java/com/wxm/keepaccount/MainActivity.java
+++ app/src/main/java/com/wxm/keepaccount/MainActivity.java
@@ -1,7 +1,9 @@
 package com.wxm.keepaccount;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    /*
+        记录收入
+     */
+    public void onClickIncomeRecord(View view) {
+        // Do something in response to button click
+        Intent intent=new Intent(this, IncomeRecordActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    /*
+        记录支出
+     */
+    public void onClickPayRecord(View view) {
+        // Do something in response to button click
+        Intent intent=new Intent(this, PayRecordActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+
+    /*
+        其它activity返回结果
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
