42f576d15586a7ec9ee49a08166f6971fdd6e924
air_flyer <fly_in_wind@qq.com>
2016-08-31 21:42:23 +0800

    small refine

---
+++ app/src/main/java/com/wxm/KeepAccount/ui/activities/ACBudgetManager.java
@@ -0,0 +1,15 @@
+package com.wxm.KeepAccount.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wxm.KeepAccount.R;
+
+public class ACBudgetManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_budget_manager);
+    }
+}
