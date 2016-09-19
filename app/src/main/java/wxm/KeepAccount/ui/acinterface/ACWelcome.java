b8c8a87fd4e49284f1e6acd7bb90934f43e684f7
air_flyer <fly_in_wind@qq.com>
2016-09-19 14:42:37 +0800

    for new version

--- app/src/main/java/wxm/KeepAccount/ui/acinterface/ACWelcome.java
+++ app/src/main/java/wxm/KeepAccount/ui/acinterface/ACWelcome.java
@@ -2,7 +2,6 @@ package wxm.KeepAccount.ui.acinterface;
 
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -11,19 +10,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.Calendar;
-import java.util.Locale;
-
 import cn.wxm.andriodutillib.util.UtilFun;
-import wxm.KeepAccount.Base.data.AppGobalDef;
 import wxm.KeepAccount.R;
-import wxm.KeepAccount.ui.acutility.ACNoteEdit;
 
 /**
  * 用户登陆后首页面
@@ -42,13 +34,6 @@ public class ACWelcome extends AppCompatActivity
         init_component();
     }
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.acbar_back_help, menu);
-        return true;
-    } */
 
     @Override
     public void onBackPressed() {
@@ -60,36 +45,6 @@ public class ACWelcome extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
-
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.acb_mi_leave : {
-                int ret_data = AppGobalDef.INTRET_USR_LOGOUT;
-
-                Intent data = new Intent();
-                setResult(ret_data, data);
-                finish();
-            }
-            break;
-
-            case R.id.acb_mi_help : {
-                Intent intent = new Intent(this, ACHelp.class);
-                intent.putExtra(ACHelp.STR_HELP_TYPE, ACHelp.STR_HELP_MAIN);
-
-                startActivityForResult(intent, 1);
-            }
-            break;
-
-            default:
-                return super.onOptionsItemSelected(item);
-
-        }
-
-        return true;
-    } */
-
 
     private void init_component() {
         // set nav view
@@ -109,125 +64,11 @@ public class ACWelcome extends AppCompatActivity
         assert null != nv;
         nv.setNavigationItemSelectedListener(this);
 
-        // init datashow
-        Button bt_datashow = UtilFun.cast(findViewById(R.id.bt_lookdata));
-        assert null != bt_datashow;
-        bt_datashow.setOnClickListener(this);
-
-        Drawable dr = bt_datashow.getCompoundDrawables()[1];
-        dr.setBounds(0, 0, BTDRAW_WIDTH, BTDRAW_HEIGHT);
-        bt_datashow.setCompoundDrawables(null, dr, null, null);
-
-        // init setting
-        Button bt_setting = UtilFun.cast(findViewById(R.id.bt_setting));
-        assert null != bt_setting;
-        bt_setting.setOnClickListener(this);
-
-        // init add income
-        Button bt_addincome = UtilFun.cast(findViewById(R.id.bt_add_income));
-        assert null != bt_addincome;
-        bt_addincome.setOnClickListener(this);
-
-        dr = bt_addincome.getCompoundDrawables()[1];
-        dr.setBounds(0, 0, BTDRAW_WIDTH, BTDRAW_HEIGHT);
-        bt_addincome.setCompoundDrawables(null, dr, null, null);
-
-        // init add pay
-        Button bt_addpay = UtilFun.cast(findViewById(R.id.bt_add_pay));
-        assert null != bt_addpay;
-        bt_addpay.setOnClickListener(this);
-
-        dr = bt_addpay.getCompoundDrawables()[1];
-        dr.setBounds(0, 0, BTDRAW_WIDTH, BTDRAW_HEIGHT);
-        bt_addpay.setCompoundDrawables(null, dr, null, null);
-
-        // init leave login
-        Button bt_leave_login = UtilFun.cast(findViewById(R.id.bt_leave_login));
-        assert null != bt_leave_login;
-        bt_leave_login.setOnClickListener(this);
-
-        dr = bt_leave_login.getCompoundDrawables()[1];
-        dr.setBounds(0, 0, BTDRAW_WIDTH, BTDRAW_HEIGHT);
-        bt_leave_login.setCompoundDrawables(null, dr, null, null);
-
-        // init edit budget
-        Button bt_edit_budget = UtilFun.cast(findViewById(R.id.bt_edit_budget));
-        assert null != bt_edit_budget;
-        bt_edit_budget.setOnClickListener(this);
-
-        dr = bt_edit_budget.getCompoundDrawables()[1];
-        dr.setBounds(0, 0, BTDRAW_WIDTH, BTDRAW_HEIGHT);
-        bt_edit_budget.setCompoundDrawables(null, dr, null, null);
-
-        // init edit remind
-        Button bt_edit_remind = UtilFun.cast(findViewById(R.id.bt_edit_remind));
-        assert null != bt_edit_remind;
-        bt_edit_remind.setOnClickListener(this);
-
-        dr = bt_edit_remind.getCompoundDrawables()[1];
-        dr.setBounds(0, 0, BTDRAW_WIDTH, BTDRAW_HEIGHT);
-        bt_edit_remind.setCompoundDrawables(null, dr, null, null);
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        switch (id) {
-            case R.id.bt_lookdata : {
-                Intent intent = new Intent(this, ACNoteShow.class);
-                startActivityForResult(intent, 1);
-            }
-            break;
-
-            case R.id.bt_edit_remind :  {
-                Intent intent = new Intent(this, ACRemindShow.class);
-                startActivityForResult(intent, 1);
-            }
-            break;
-
-            /*
-            case R.id.bt_edit_budget: {
-                Intent intent = new Intent(this, ACBudgetShow.class);
-                startActivityForResult(intent, 1);
-            }
-            break;
-            */
-
-            case R.id.bt_setting : {
-                Toast.makeText(getApplicationContext(), "invoke setting!", Toast.LENGTH_SHORT).show();
-            }
-            break;
-
-            case R.id.bt_add_income :
-            case R.id.bt_add_pay : {
-                Intent intent = new Intent(v.getContext(), ACNoteEdit.class);
-                intent.putExtra(ACNoteEdit.PARA_ACTION, ACNoteEdit.LOAD_NOTE_ADD);
-
-                Calendar cal = Calendar.getInstance();
-                cal.setTimeInMillis(System.currentTimeMillis());
-                intent.putExtra(AppGobalDef.STR_RECORD_DATE,
-                        String.format(Locale.CHINA
-                                ,"%d-%02d-%02d"
-                                ,cal.get(Calendar.YEAR)
-                                ,cal.get(Calendar.MONTH) + 1
-                                ,cal.get(Calendar.DAY_OF_MONTH)));
-
-                startActivityForResult(intent, 1);
-            }
-            break;
-
-            case R.id.bt_leave_login :  {
-                int ret_data = AppGobalDef.INTRET_USR_LOGOUT;
-
-                Intent data = new Intent();
-                setResult(ret_data, data);
-                finish();
-            }
-            break;
-
-            default:
-                Log.e(TAG, "view(id :" + id + ")的click未处理");
-        }
     }
 
     @Override
