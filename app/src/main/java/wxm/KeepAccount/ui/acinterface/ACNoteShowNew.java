e60a6f8a31ac0828b0f2ec4d69b4927feb1eed6f
wxm <fly_in_wind@qq.com>
2016-09-25 17:09:05 +0800

    Merge branch 'at_homepc' of https://code.csdn.net/air_flyer/andriod_keepaccount
    # Conflicts:
    #	app/src/main/java/com/wxm/KeepAccount/Base/db/DBOrmliteHelper.java
    #	app/src/main/java/wxm/KeepAccount/ui/acutility/ACRecordTypeEdit.java
    #	app/src/main/res/layouts/listview/layout/li_record_type.xml

--- app/src/main/java/wxm/KeepAccount/ui/acinterface/ACNoteShowNew.java
+++ app/src/main/java/wxm/KeepAccount/ui/acinterface/ACNoteShowNew.java
@@ -1,14 +1,39 @@
 package wxm.KeepAccount.ui.acinterface;
 
+import android.app.ActivityGroup;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.LocalActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.widget.TabHost;
 
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+
 import wxm.KeepAccount.R;
+import wxm.KeepAccount.ui.acutility.ACDailyShow;
+import wxm.KeepAccount.ui.fragment.ShowData.TabFragment1;
+import wxm.KeepAccount.ui.fragment.ShowData.TabFragment2;
+import wxm.KeepAccount.ui.fragment.ShowData.TabFragment3;
 
 public class ACNoteShowNew extends AppCompatActivity {
     private final static String TAG = "ACNoteShowNew";
-    private TabHost     mTHTabs;
+
+    protected final static String TAB_DAILY     = "日统计";
+    protected final static String TAB_MONTHLY   = "月统计";
+    protected final static String TAB_YEARLY    = "年统计";
+
+    private float       mLastX;
+    private TabLayout   mTLTabs;
+    private ViewPager   mVPTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +44,105 @@ public class ACNoteShowNew extends AppCompatActivity {
     }
 
     private void init_tabs() {
-        // 获取TabHost对象
-        mTHTabs = (TabHost) findViewById(R.id.th_tabs);
-        mTHTabs.setup();
-        mTHTabs.addTab(mTHTabs.newTabSpec("tab1").setIndicator("第一个标签")
-                .setContent(R.id.tab1));
+        mTLTabs = (TabLayout) findViewById(R.id.tl_tabs);
+        mTLTabs.addTab(mTLTabs.newTab().setText(TAB_DAILY));
+        mTLTabs.addTab(mTLTabs.newTab().setText(TAB_MONTHLY));
+        mTLTabs.addTab(mTLTabs.newTab().setText(TAB_YEARLY));
+        mTLTabs.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        mTHTabs.addTab(mTHTabs.newTabSpec("tab2").setIndicator("第二个标签")
-                .setContent(R.id.tab2));
+        mVPTabs = (ViewPager) findViewById(R.id.tab_pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), mTLTabs.getTabCount());
+        mVPTabs.setAdapter(adapter);
+        mVPTabs.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTLTabs));
+        mTLTabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mVPTabs.setCurrentItem(tab.getPosition());
+            }
 
-        mTHTabs.addTab(mTHTabs.newTabSpec("tab3").setIndicator("第三个标签")
-                .setContent(R.id.tab3));
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    /*
+    @Override
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+            // when user first touches the screen to swap
+            case MotionEvent.ACTION_DOWN: {
+                mLastX = touchevent.getX();
+            }
+            break;
+
+            case MotionEvent.ACTION_UP: {
+                float currentX = touchevent.getX();
+                if(currentX != mLastX)
+                    switchTabs(mLastX > currentX);
+            }
+            break;
+        }
+        return false;
+    }
+
+    /**
+     * 切换tabhost当前标签页
+     * @param direction     如向右移动则为false, 否则为true
+    public void switchTabs(boolean direction) {
+        int cur_pos = mTLTabs.getSelectedTabPosition();
+        int tab_sz = mTLTabs.getTabCount();
+        int n_pos = direction ?
+                        cur_pos == 0 ? tab_sz - 1 : cur_pos - 1
+                        : cur_pos != (tab_sz - 1) ? cur_pos + 1 : 0;
+
+        mTLTabs.setScrollPosition(n_pos, 0, true);
+    }
+    */
+
+
+    public class PagerAdapter extends FragmentStatePagerAdapter {
+        int mNumOfTabs;
+
+        public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+            super(fm);
+            this.mNumOfTabs = NumOfTabs;
+        }
+
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            android.support.v4.app.Fragment fr;
+            switch (position) {
+                case 0:
+                    fr = new TabFragment1();
+                    break;
+
+                case 1:
+                    fr = new TabFragment2();
+                    break;
+
+                case 2:
+                    fr = new TabFragment3();
+                    break;
+
+                default:
+                    fr = null;
+                    break;
+            }
+
+            return fr;
+        }
+
+        @Override
+        public int getCount() {
+            return mNumOfTabs;
+        }
     }
 }
--- app/src/main/java/wxm/KeepAccount/ui/acutility/ACDailyShow.java
+++ app/src/main/java/wxm/KeepAccount/ui/acutility/ACDailyShow.java
@@ -2,12 +2,24 @@ package wxm.KeepAccount.ui.acutility;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import wxm.KeepAccount.R;
 
 public class ACDailyShow extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.ac_daily_show);
     }
 }
