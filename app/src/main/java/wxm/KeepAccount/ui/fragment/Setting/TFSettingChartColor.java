bfc3b2166051b446f9d60c4769678a6bb344c063
air_flyer <fly_in_wind@qq.com>
2016-10-10 18:39:02 +0800

    small refine

--- app/src/main/java/wxm/KeepAccount/ui/fragment/Setting/TFSettingChartColor.java
+++ app/src/main/java/wxm/KeepAccount/ui/fragment/Setting/TFSettingChartColor.java
@@ -1,9 +1,24 @@
 package wxm.KeepAccount.ui.fragment.Setting;
 
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import wxm.KeepAccount.R;
+
 /**
  * 图表颜色设置页面
  * Created by 123 on 2016/10/10.
  */
 public class TFSettingChartColor extends TFSettingBase {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.vw_setting_chart_color, container, false);
+        return v;
+    }
+
 }
