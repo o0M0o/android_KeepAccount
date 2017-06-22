ed50b31929fbbc3f98f4ba2b262cb84d939c7d6a
hugo <fly_in_wind@qq.com>
2017-06-22 15:31:10 +0800

    small refine

--- app/src/main/assets/js/reportDay.js
+++ app/src/main/assets/js/reportDay.js
@@ -1,11 +1,24 @@
 /**
 *   生成日报页面
 */
+
+
 var vueApp = new Vue({
     el:'#app',
     data:{
         days:[],
         items:[]
+    },
+    updated : function()    {
+        var tr = document.getElementsByClassName("trClass")
+        for (var i = 0, len = tr.length; i < len; i++) {
+            tr[i].style.backgroundColor = tr[i].classList.contains("pay_td") ? "#FFE7BA" : "#F0FFF0"
+        }
+
+        var td = document.getElementsByClassName("day_td")
+        for (var i = 0, len = td.length; i < len; i++) {
+            td[i].style.backgroundColor = 0 !== i % 2 ? "#f9f9f9" : "#ffffff"
+        }
     }
 })
 
